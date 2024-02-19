3b088f933e847120b91412e706454d7db064c906
James <james.jasenia@gmail.com>
2024-02-19 20:39:16 +1100

    Fixed typo.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const express         = require('express');
-const ip = require('ip');
-const dotenv = require('dotenv');
+const express = require("express");
+const ip = require("ip");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -8,16 +8,15 @@ const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => { 
-    res.send(
-        {
-            message: 'UP'
-        }
-    )
+app.get("/", (req, res) => {
+  res.send({
+    message: "UP",
+  });
 });
 
 console.log(`This is the environment:`);
 console.log(process.env);
 
-
-app.listen(PORT, () => console.log(`Server running on: ${ip.address()}:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on: ${ip.address()}:${PORT}`)
+);
