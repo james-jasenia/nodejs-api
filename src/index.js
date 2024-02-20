8036c88cbcd92d76d89ca91551978fa3fabed53d
James <james.jasenia@gmail.com>
2024-02-20 20:28:22 +1100

    Added HttpStatus objects.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ip from "ip";
 import dotenv from "dotenv";
 import Response from './domain/response.js';
 import logger from './util/logger.js';
+import HttpStatus from "./controller/patient.controller.js";
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send(new Response(200, 'OK', 'Patient API, v1.0.0', {
+  res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0', {
     patients: {
         name: "Junior"
     }
