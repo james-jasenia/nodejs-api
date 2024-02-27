d9ef70e879247652964e69d40c2bf2aad08f301f
James <james.jasenia@gmail.com>
2024-02-27 20:43:00 +1100

    Updated not found path.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import Response from './domain/response.js';
 import logger from './util/logger.js';
 import HttpStatus from "./controller/patient.controller.js";
+import patientRoutes from "./route/patient.route.js";
 
 dotenv.config();
 
@@ -11,12 +12,15 @@ const PORT = process.env.SERVER_PORT || 3000;
 const app = express();
 app.use(express.json());
 
+app.use('/patients', patientRoutes);
+
 app.get("/", (req, res) => {
-  res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0', {
-    patients: {
-        name: "Junior"
-    }
-  }));
+  res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0'));
+});
+
+app.all("*", (req, res) => { 
+  res.status(HttpStatus.NOT_FOUND.code)
+     .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist.'));
 });
 
 app.listen(PORT, () =>
